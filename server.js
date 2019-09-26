dfb0bba349842e044cc2aaf3d3d9738f8b0a6806
unknown <sallamtanna2015@hotmail.com>
2019-09-26 12:17:00 +0300

    Add the whole application

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+
+var app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.post('/update', function(req, res) {
+    pg.connect(process.env.DATABASE_URL, function (err, conn, done) {
+        // watch for any connect issues
+        if (err) console.log(err);
+
+
+        //
+        // conn.query(
+        //     'SELECT Question_1__c FROM  salesforce.application__c ',
+        //
+        //     function(err, result) {
+        //         if (err != null || result.rowCount == 0) {
+        //           console.log('some error');
+        //         }
+        //         else {
+        //             console.log('eeeeeee', result);
+        //             res.send(result);
+        //             done();
+        //         }
+        //     }
+        // );
+        conn.query('SELECT Question_1__c FROM  salesforce.application__c', function(err, result) {
+          console.log('2. select query');
+          if (err) {return console.error('Errrror:', err)}  
+          console.log("totalllllllll : " + result.json());
+        });
+
+
+
+
+    });
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
